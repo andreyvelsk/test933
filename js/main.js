b3b80a01b4ee8209445972a9d71cca507f85a5b1
andrey_velsk <andrey_velsk@mail.ru>
2018-09-12 18:24:24 +0300

    Добавил картинки, анимацию

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+
+$fixed = $("#btncheck"), //меню десктоп
+
+console.log($fixed);
+
+$fixed.on("click", function (event) {
+	//отменяем стандартную обработку нажатия по ссылке
+	event.preventDefault();
+
+	//забираем идентификатор бока с атрибута href
+	var id  = $(this).attr('href'),
+
+	//узнаем высоту от начала страницы до блока на который ссылается якорь
+	top = $(id).offset().top;
+
+		//анимируем переход на расстояние - top
+	$('body,html').animate({scrollTop: top}, 700);
+
+	});      
+
+});
